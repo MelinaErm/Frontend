75b47bb7d00635a1f05be356570395233d342659
Melina <melinaermeidi@yahoo.gr>
2024-03-28 23:09:25 +0200

    commit 2

--- app/src/main/java/com/example/frontend/MainActivity.kt
+++ app/src/main/java/com/example/frontend/MainActivity.kt
@@ -11,6 +11,9 @@ import android.view.Menu
 import android.view.MenuItem
 import com.example.frontend.databinding.ActivityMainBinding
 
+import android.widget.AutoCompleteTextView
+import android.widget.ArrayAdapter
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -29,7 +32,36 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
         }
+
+        //!!! changes
+        //initialize AutoCompleteTextView
+        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.auto_complete_text_view)
+
+        // an example location suggestions with type of event and title
+        val locationsWithDetails = arrayOf(
+            "Athens - Music - 'Music Festival'",
+            "Athens - Food - 'Food Festival'",
+            "Thessaloniki - Art - 'Art Festival'"
+        )
+
+        //set up ArrayAdapter for AutoCompleteTextView
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, locationsWithDetails)
+        autoCompleteTextView.setAdapter(adapter)
+
+        //set up item click listener for AutoCompleteTextView
+        autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            //split the selected item to extract location, event type, and title
+            val details = selectedItem.split(" - ")
+            val location = details[0]
+            val eventType = details[1]
+            val eventTitle = details[2]
+            //perform any action here when a location is selected
+            Snackbar.make(view, "Location: $location\nType: $eventType\nTitle: $eventTitle", Snackbar.LENGTH_LONG).show()
+        }
+        //!!!! end
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
